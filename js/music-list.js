b65f3b2162098c68640b4a146774d355d915e795
Manel Ariapala <manelariapala@gmail.com>
2021-10-20 18:01:42 +0200

    completed the functionality of the player

---
+++ js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+
+    {
+        name: "Home",
+        artist: "Edward Sharpe and the Magnetic Zero",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Island in the sun",
+        artist: "Weezer",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Never Forget You",
+        artist: "The Noisette",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Shiny Happy People",
+        artist: "R.E.M",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Way Down We Go",
+        artist: "Kaleo",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Summer Wine",
+        artist: "Lana Del Rey",
+        img: "music-6",
+        src: "music-6"
+    },
+];
